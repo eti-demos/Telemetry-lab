0e54a3607bb3202830c4a4b86ec2958ca1ec32a4
cywu <wu.chgyen@gmail.com>
2023-11-29 10:31:44 +0100

    Wasm filter is able to export data to elastic search

--- content/elasticsearch/shared/wasm/main.go
+++ content/elasticsearch/shared/wasm/main.go
@@ -64,7 +64,7 @@ type httpContext struct{
 }
 
 type Telemetry struct {
-    apiLogId uuid.UUID
+    apiLogId uuid.UUID      `json:"apilogid, omitempty"`
     Depth       int         `json:"depth, omitempty"`
     RequestID   string      `json:"requestID,omitempty"`
 }
@@ -119,22 +119,36 @@ func (ctx *httpContext) OnHttpRequestHeaders(numHeaders int, endOfStream bool) t
 }
 
 
-
+const jsonPayload string = `{"requestID":"%v","depth":"%v", "apilogid": "%v"}`
+// var emptyTrailers = [][2]string{}
 
 func (ctx *httpContext) OnHttpStreamDone() {
 
 	proxywasm.LogInfof("Telemetry dispatched")
+    body := fmt.Sprintf(jsonPayload,
+		ctx.RequestID, ctx.Depth, ctx.apiLogId)
 
-	// if err := sendAuthPayload(&ctx.Telemetry, ctx.serverAddress); err != nil {
-	if err := sendAuthPayload(&ctx.Telemetry, "elasticsearch"); err != nil {
-		proxywasm.LogErrorf("Failed to send payload. %v", err)
-	}
+    asHeader := [][2]string{{":method", "POST"}, {":authority", "elasticsearch:9200"},
+        {":path", "/api-traffic-log/_doc"}, {"accept", "*/*"}, {"Content-Type",
+            "application/json"}}
+
+    _, err := proxywasm.DispatchHttpCall("elasticsearch", asHeader, []byte(body),
+        nil, httpCallTimeoutMs, httpCallResponseCallback); 
+
+    if err != nil {
+        proxywasm.LogErrorf("Dispatch httpcall failed. %v", err) 
+        // return err 
+    }
+
+	// // if err := sendAuthPayload(&ctx.Telemetry, ctx.serverAddress); err != nil {
+	// if err := sendAuthPayload(&ctx.Telemetry, "elasticsearch"); err != nil {
+	// 	proxywasm.LogErrorf("Failed to send payload. %v", err)
+	// }
+
 }
 
-const jsonPayload string = `{"requestID":"%v","scheme":"%v", "Api-Log-Id": "%v"}`
 
 
-var emptyTrailers = [][2]string{}
 
 const (
 	httpCallTimeoutMs = 15000
@@ -148,39 +162,46 @@ const (
 
 
 
-func sendAuthPayload(payload *Telemetry, clusterName string) error {
-
-    body := fmt.Sprintf(jsonPayload,
-		payload.RequestID, payloajd.Depth, payload.apiLogId)
-
-
-    asHeader := [][2]string{{":method", "POST"}, {":authority", "apiclarity"},
-        {":path", "/api-traffic-log/_doc"}, {"accept", "*/*"}, {"Content-Type",
-            "application/json"}, {"x-request-id", payload.RequestID}}
-
-    _, err := proxywasm.DispatchHttpCall(clusterName, asHeader, []byte(body),
-        emptyTrailers, httpCallTimeoutMs, httpCallResponseCallback); 
-
-    if err != nil {
-        proxywasm.LogErrorf("Dispatch httpcall failed. %v", err) 
-        return err 
-    }
-
-	proxywasm.LogInfof("Telemetry dispatched")
-	return nil
-}
+// func sendAuthPayload(payload *Telemetry, clusterName string) error {
+//
+//     body := fmt.Sprintf(jsonPayload,
+// 		payload.RequestID, payload.Depth, payload.apiLogId)
+//
+//
+    // asHeader := [][2]string{{":method", "POST"}, {":authority", "elasticsearch:9200"},
+    //     {":path", "/api-traffic-log/_doc"}, {"accept", "*/*"}, {"Content-Type",
+    //         "application/json"}, {"x-request-id", payload.RequestID}}
+//
+//     _, err := proxywasm.DispatchHttpCall(clusterName, asHeader, []byte(body),
+//         emptyTrailers, httpCallTimeoutMs, httpCallResponseCallback); 
+//
+//     if err != nil {
+//         proxywasm.LogErrorf("Dispatch httpcall failed. %v", err) 
+//         return err 
+//     }
+//
+// 	proxywasm.LogInfof("Telemetry dispatched")
+// 	return nil
+// }
 
 func httpCallResponseCallback(numHeaders, bodySize, numTrailers int) {
+    proxywasm.LogWarnf("Callback")
+
 	proxywasm.LogDebugf("httpCallResponseCallback. numHeaders: %v, bodySize: %v, numTrailers: %v", numHeaders, bodySize, numTrailers)
 	headers, err := proxywasm.GetHttpCallResponseHeaders()
 	if err != nil {
 		proxywasm.LogWarnf("Failed to get http call response headers. %v", err)
 		return
 	}
-	for _, header := range headers {
-		if header[0] == statusCodePseudoHeaderName {
-			proxywasm.LogDebugf("Got response status from trace server: %v", header[1])
-		}
+
+    for _, h := range headers {
+		proxywasm.LogInfof("request header --> %s: %s", h[0], h[1])
 	}
+
+	// for _, header := range headers {
+	// 	if header[0] == statusCodePseudoHeaderName {
+	// 		proxywasm.LogDebugf("Got response status from trace server: %v", header[1])
+	// 	}
+	// }
 }
 
